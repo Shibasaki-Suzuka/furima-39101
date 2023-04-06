60ce72f64dc4d7566ab53b4b87235ba0dceb2670
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-04-06 19:20:57 +0900

    テストコード設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters.'}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters.'}
   validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
   validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
   validates :birthday, presence: true      
---
+++ spec/factories/users.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.free_email}
+    password              {'1a'+Faker::Internet.password(min_length: 6)}
+    encrypted_password    {password}
+    last_name             {Gimei.last.kanji}
+    first_name            {Gimei.first.hiragana}
+    last_name_kana        {Gimei.last.katakana}
+    first_name_kana       {Gimei.first.katakana}
+    birthday              {'1948-02-19'}
+
+
+  end
+end

e2ab4b89a56aee3d7fb307e06cc92c6ee6a14987
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-04-07 15:46:13 +0900

    create定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,14 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
 end

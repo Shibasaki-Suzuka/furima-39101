4efd044299b143174cff0bd3fe42f701cd7ffc63
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-04-11 20:55:09 +0900

    before_actionの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :move_to_edit, only: [:edit, :update]
+
   def index
     @items = Item.order('created_at DESC')
   end
@@ -25,12 +27,24 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :sentence, :category_id, :status_id, :postage_burden_id, :prefecture_id,
                                  :shipping_day_id, :selling_price).merge(user_id: current_user.id)
+  end
+
+  def move_to_edit
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user == @item.user
   end
 end

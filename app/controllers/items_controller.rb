6cd630e69a499e5ccf37867c4c5d2c67220dd68b
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-04-11 19:34:44 +0900

    編集機能の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,12 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  
+
   private
 
   def item_params
